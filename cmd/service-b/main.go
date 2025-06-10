7374f50b45d3484f5d8d7b99c23f5c7bb1523ea1
AbdulBari <bay7amov@gmail.com>
2025-06-10 20:57:51 +0300

    Добавил Consumer

--- cmd/service-b/main.go
+++ cmd/service-b/main.go
@@ -1,1 +1,54 @@
-package serviceb
+package main
+
+import (
+	"os"
+	"transaction-worker/internal/common/config"
+	"transaction-worker/internal/common/logger"
+	"transaction-worker/internal/common/rabbitmq"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	logger.Init()
+	defer logger.Sync()
+
+	cfg := config.MustLoad()
+	logger.Log.Infof("env: %s", cfg.Env)
+
+	if err := godotenv.Load(); err != nil {
+		panic("No .env file found")
+	}
+	amqpServerURL := os.Getenv("AMQP_SERVER_URL")
+
+	rabbitClinet, err := rabbitmq.NewRabbitMQ(amqpServerURL)
+	if err != nil {
+		logger.Log.Fatal(err)
+	}
+	defer rabbitClinet.Close()
+
+	if err := rabbitClinet.DeclareQueue("QueueService1"); err != nil {
+		logger.Log.Fatal("Failed to declare queue: ", err)
+	}
+
+	messages, err := rabbitClinet.ConsumeMessages("QueueService1")
+
+	if err != nil {
+		logger.Log.Fatalf("Error consuming messages: %v", err)
+	}
+
+	logger.Log.Info("Successfully connected to RabbitMQ")
+	logger.Log.Info("Waiting for messages")
+
+	forever := make(chan bool)
+
+	go func() {
+		for message := range messages {
+			logger.Log.Infof(" > Received message: %s\n", message.Body)
+
+			message.Ack(false)
+		}
+	}()
+
+	<-forever
+}
